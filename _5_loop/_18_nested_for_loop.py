7e575fbbf6e873c7ef767a5d5be9a2b5ae1b2b87
Sherly14 <bharatiyadav14@gmail.com>
2022-09-16 00:08:08 +0530

    string

---
+++ _5_loop/_18_nested_for_loop.py
@@ -0,0 +1,24 @@
+"""
+
+****
+****
+****
+****
+
+"""
+
+# outer for loop - row
+# inner for loop - column
+
+for i in range(4):      # i = 4
+    for j in range(4):  # j = 0
+        print("*",end="")
+    print("\n",end="\n")
+
+
+
+
+
+
+
+
--- _6_string/_2_inbuilt_str_methods.py
+++ _6_string/_2_inbuilt_str_methods.py
@@ -1,31 +1,92 @@
-str1 = "Python is A proGramming language"
+# str1 = "Python is a programming language"
+# print("Original : ",str1)
 
 # capital = str1.capitalize() # converts initial character into caps
-# print(capital)
+# print("Capitalize : ",capital)
 
 # upper_case = str1.upper()   # converts every characters into caps
-# print(upper_case)
+# print("Upper :",upper_case)
 
 # lower_case = str1.lower()   # converts every characters into small letters
-# print(lower_case)
+# print("Lower :",lower_case)
 
 # swap = str1.swapcase()      # swaps the case of characters
-# print(swap)
+# print("Swapcase :",swap)
 
-# ends_with = str1.endswith("uage") # checks the end of string
-# print(ends_with)
+# ends_with = str1.endswith("proGramming language") # checks the end of string
+# print("Endswith :",ends_with)
 
 # starts_with = str1.startswith("Python") # checks the start of the string
-# print(starts_with) 
+# print("Startswith :",starts_with) 
 
 # is_upper = str1.isupper() # checks if every character of string is capital
-# print(is_upper)
+# print("Isupper :",is_upper)
 
 # is_lower = str1.islower() # checks if every character of string is lower
-# print(is_lower)
+# print("Islower :",is_lower)
 
-# title_demo = str1.title()  # converts initial letter of every character into capital
-# print(title_demo)
+# title_demo = str1.title()  # converts initial letter of every word into capital
+# print("Title :",title_demo)
 
 # is_title = str1.istitle() # checks if it follows title pattern
-# print(is_title)
+# print("IsTitle :",is_title)
+
+# count_demo = str1.count('is') # returns the count of value passed
+# print("Count : ",count_demo)
+
+# index_demo = str1.index('g') # returns the index of the value passed
+# print("Index : ",index_demo)
+
+# length = len(str1)           # returns length of characters
+# print("Length : ",length)
+
+# strip_demo = str1.strip()
+# print("Strip : ",strip_demo)
+
+# lstrip_demo = str1.lstrip()
+# print("Lstrip : ",lstrip_demo)
+
+# rstrip_demo = str1.rstrip()
+# print("Rstrip : ",rstrip_demo)
+
+# replace_demo = str1.replace("a","o")
+# print("Replace : ",replace_demo)
+
+# replace_demo = str1.replace("Python is","Java in")
+# print("Replace : ",replace_demo)
+
+# split_demo = str1.split("a")
+# print("Split : ",split_demo,type(split_demo))
+
+# for string in split_demo:
+#     print(string)
+
+from ntpath import join
+
+
+str1 = "Programming Language"
+print("Original : ",str1)
+
+# is_digit = str1.isdigit()
+# print("Is digit : ",is_digit)
+
+# is_alpha = str1.isalpha()
+# print("Is alpha :",is_alpha)
+
+# is_alnum = str1.isalnum() # a-z,A-Z,1-9
+# print("Is Alnum : ",is_alnum)
+
+# ord_demo = ord('r') # returns ascii value of the value passed
+# print(ord_demo)
+
+# American Standard Code for Information 
+
+# find_demo  = str1.find('a')
+# print("Find : ",find_demo)
+
+# find_demo  = str1.find('a',16,24)
+# print("Find : ",find_demo)
+
+# lst = ["cat","dog","rat"]
+# join_demo = "".join(lst)
+# print(join_demo)
