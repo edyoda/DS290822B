b5d2233c35b9ae529cab0497643c93ff00b446d1
Sherly14 <bharatiyadav14@gmail.com>
2022-10-21 21:40:31 +0530

    linkedlist

--- DSA/_7_linkedlist.py
+++ DSA/_7_linkedlist.py
@@ -1,36 +1,95 @@
 class node:
-    def __init__(self,data,link=None):
-        self.data = data
-        self.link = link
+    def __init__(self,data,link = None):
+        self.data = data # element
+        self.link = link # memory location
 
 class linkedlist:
     def __init__(self,head=None):
         self.head = head
 
     def is_empty(self):
-        if self.head == None:
+        if self.head is None:
             return True
-            
-    def append(self,element):
-        new_node = node(element)
-        if self.is_empty:
-            self.head = new_node
-        else:
-            temp = self.head
-            while temp.link != None:
-                temp = temp.link
-            temp.link = new_node
-        print("Node have been inserted")
+
+    def append(self,data):
+        new_node = node(data,self.head) # data - 10 , None - link
+        self.head = new_node # data - 10 ----> 1001 - link
 
     def display(self):
-        pass
+        itr = self.head
+        while itr:
+            print(itr.data)   
+            itr = itr.link    
+
+    def size(self):
+        count = 0
+        itr = self.head
+        while itr:
+            count += 1            
+            itr = itr.link
+        return count
+
+    def appendleft(self,element):
+        if self.is_empty():
+            self.head = node(element)
+            return
+
+        itr = self.head
+        while itr.link:
+            itr = itr.link
+        itr.link = node(element)
+
+    def insert(self,index,data):
+        if index < 0 or index > self.size :
+            raise Exception("Invalid Index Error")
+
+        if index == 0:
+            self.append(data)
+            return
+
+        itr = self.head
+        count = 0
+        while itr:
+            if count == index - 1:
+                node1 = node(data,itr.link)
+                itr.link = node1
+            itr = itr.link
+            count += 1
+
+    def remove(self,index):
+        if index < 0 or index > self.size() :
+            raise Exception("Invalid Index Error")
+
+        if index == 0:
+            self.head = self.head.link
+            return
+
+        itr = self.head
+        count = 0
+        while itr:
+            if count == index - 1:
+                itr.link = itr.link.link
+            itr = itr.link
+            count += 1
+
+
+
+
 
 if __name__ == "__main__":
     obj = linkedlist()
-    obj.append(10)
-    obj.append(20)
-    obj.append(30)
+    # obj.append(10)
+    # obj.append(20)
+    # obj.append(30)
+    # obj.display()
+    # print(obj.size())
 
-        
+    obj.appendleft(10)
+    obj.appendleft(20)
+    obj.appendleft(30)
+    # obj.insert(2,100)
+    obj.remove(1)
+    obj.display()
+
 
 
---
+++ DSA/_8_problem_solving/sum_of_pairs.py
@@ -0,0 +1,14 @@
+a=[1,2,3,4,5,6,7,8,9]
+data=4
+# for i in a:
+#   for j in a:
+#     if i+j==data:
+#       print('the value =',i,j)
+
+# lst = []
+for i in range(len(a)):
+    for j in range(i,len(a)):
+        if (a[i]+a[j])==data:
+            # .append([a[i],a[j]])
+            print('the value =',a[i],a[j])
+
