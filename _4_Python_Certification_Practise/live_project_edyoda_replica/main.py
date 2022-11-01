285ba4de88754f3c3d12c5e61290a4c49ddc5a8b
Sherly14 <bharatiyadav14@gmail.com>
2022-11-01 22:11:27 +0530

    edyoda project

--- _4_Python_Certification_Practise/live_project_edyoda_replica/main.py
+++ _4_Python_Certification_Practise/live_project_edyoda_replica/main.py
@@ -1,6 +1,6 @@
 import re
 from operations import *
-
+import random
 
 if __name__ == "__main__":
     while True:
@@ -17,7 +17,7 @@ if __name__ == "__main__":
                 print("Please enter valid choice!")
                 continue
 
-            if register_choice:
+            if register_choice == 1:
                 name = input("Enter your name : ")                
                 mobile = input("Enter your mobile : ")
                 email = input("Enter your email : ")                
@@ -29,12 +29,12 @@ if __name__ == "__main__":
                 password_re = re.findall(r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{6,20}$" , password)
 
                 if name_re and mobile_re and email_re and password_re:
-                    flag = register("manager","manager.json",name,mobile,email,password)
+                    flag = register("manager.json",name,mobile,email,password)
                     
                     if flag:
-                        print("Successfully Added")
+                        print("Successfully Registered!")
                     else:
-                        print("Failed to add the details")
+                        print("Registeration Unsuccessful")
                 else:
                     if not name_re:
                         print("Entered name format is incorrect")
@@ -48,3 +48,94 @@ if __name__ == "__main__":
                     if not password_re:
                         print("Entered name format is incorrect")
                         continue
+
+            if register_choice == 2:
+                name = input("Enter your name : ")                
+                mobile = input("Enter your mobile : ")
+                email = input("Enter your email : ")                
+                password = input("Enter your password : ")                
+
+                name_re = re.findall(r'^[A-Za-z]{2,15}\s[A-Za-z]{2,15}$', name)
+                mobile_re = re.findall(r'^[1-9]{1}[0-9]{9}$' , mobile)
+                email_re = re.findall("^[A-z][a-zA-Z0-9._]+[@][a-z]+[.][a-z]+$",email)
+                password_re = re.findall(r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{6,20}$" , password)
+
+                if name_re and mobile_re and email_re and password_re:
+                    flag = register("student.json",name,mobile,email,password)
+                    
+                    if flag:
+                        print("Successfully Registered!")
+                    else:
+                        print("Registeration Unsuccessful")
+                else:
+                    if not name_re:
+                        print("Entered name format is incorrect")
+                        continue
+                    if not mobile_re:
+                        print("Entered name format is incorrect")
+                        continue
+                    if not email_re:
+                        print("Entered name format is incorrect")
+                        continue
+                    if not password_re:
+                        print("Entered name format is incorrect")
+                        continue
+
+            if register_choice == 3:
+                print("***Bye****")
+                exit()
+        
+        if choice == 2:
+            login_choice = int(input("Enter \n1.Login as Admin \n2.Login Student \n3.Exit : \n"))
+            if login_choice == 1:
+                email_id = input("Enter your email ID : ")
+                password = input("Enter your password : ")
+                flag = login("manager.json",email_id,password)
+                if flag:
+                    print("Login Successful")
+                    while True:
+                        admin_choice = int(input("Enter \n1.Create Module \n2.View Module \n3.Update Module \n4.Delete Module : \n"))
+                        # Create Module
+                        if admin_choice == 1:
+                            
+                            module_ID = random.randint(10000,20000)
+                            module_name = input("Enter Module Name : ")
+                            start_date  = input("Enter Start Date (YYYY-MM-DD) : ")
+                            end_date  = input("Enter End Date (YYYY-MM-DD) : ")
+
+                            if module_ID and module_name and start_date and end_date:
+                                flag = create_module("module.json",module_ID,module_name,start_date,end_date)
+                                if flag:
+                                    print("Module is added successfully")
+                                else:
+                                    print("Module did not get added")
+                        # View Module
+                        if admin_choice == 2:
+                            data = view_module("module.json")
+                            for i in data:
+                                print(i)
+
+                        # Update Module
+                        if admin_choice == 3:
+                            module_ID = int(input("Enter Module ID : "))
+                            module_name = input("Enter new Module Name : ")
+                            start_date  = input("Enter new Start Date (YYYY-MM-DD) : ")
+                            end_date  = input("Enter new End Date (YYYY-MM-DD) : ")
+
+                            flag = update_module("module.json",module_ID,module_name,start_date,end_date)
+                            if flag:
+                                print("Module Updated")
+                            else:
+                                print("Module did not get updated!")
+
+                        # Delete Module
+                        if admin_choice == 4:
+                            module_ID = int(input("Enter Module ID : "))
+                            flag = delete_module("module.json",module_ID)
+                            if flag:
+                                print("Module Deleted")
+                            else:
+                                print("Module did not get deleted!")
+                else:
+                    print("Login Unsuccessful")
+
--- _4_Python_Certification_Practise/live_project_edyoda_replica/operations.py
+++ _4_Python_Certification_Practise/live_project_edyoda_replica/operations.py
@@ -1,35 +1,110 @@
 import json
 
-def register(type,filename,name,mobile,email,password):
-    
-    if type.lower()=='manager':
-        details={
+def register(filename,name,mobile,email,password):
+    details={
             "Full Name":name,
             "Mobile Number":mobile,
             "Email":email,
             "Password":password,
         }
-        file = open("manager.json","r+")
-        try:
-            data=json.load(file)
-            if details not in data:
-                data.append(details)
-                file.seek(0)
-                file.truncate()
-                json.dump(data,file)
-                file.close()
-                return True
-        except json.decoder.JSONDecodeError:
-            print("Exception")
-            lst=[]
-            lst.append(data)
-            json.dump(lst,file)
+        
+    file = open(filename,"r+")
+    try:
+        data=json.load(file)
+        if details not in data:
+            data.append(details)
+            file.seek(0)
+            file.truncate()
+            json.dump(data,file)
             file.close()
             return True
-        finally:
-            print("finally")
-            file.close()
+    except json.decoder.JSONDecodeError:
+        lst=[]
+        lst.append(details)
+        json.dump(lst,file)
+        file.close()
+        return True
+    finally:
+        file.close()
+    return False
+
+def login(filename,email_id,password):
+    file = open(filename,"r+")
+    try:
+        data = json.load(file)
+        for i in data:
+            if i["Email"] == email_id and i["Password"] == password:
+                return True
+            else:
+                return False
+    except json.decoder.JSONDecodeError:
         return False
+    finally:
+        file.close()
+    return False
+
+def create_module(filename,module_ID,module_name,start_date,end_date):
+    details={
+            "module_ID":module_ID,
+            "module_name":module_name,
+            "start_date":start_date,
+            "end_date":end_date,
+        }
+        
+    file = open(filename,"r+")
+    try:
+        data=json.load(file)
+        if details not in data:
+            data.append(details)
+            file.seek(0)
+            file.truncate()
+            json.dump(data,file)
+            file.close()
+            return True
+    except json.decoder.JSONDecodeError:
+        lst=[]
+        lst.append(details)
+        json.dump(lst,file)
+        file.close()
+        return True
+    finally:
+        file.close()
+    return False
+
+def view_module(filename):
+    file = open(filename,"r+")
+    data = json.load(file)
+    return data
+
+def update_module(filename,module_ID,module_name,start_date,end_date):
+    file = open(filename,"r+")
+    data = json.load(file)
+    for i in range(len(data)):
+        if data[i]["module_ID"] == module_ID:
+            data[i]["module_name"] = module_name
+            data[i]["start_date"] = start_date
+            data[i]["end_date"] = end_date
+            file.seek(0)
+            file.truncate()
+            json.dump(data,file)
+            file.close()
+            return True
+    return False
+
+def delete_module(filename,module_ID):
+    file = open(filename,"r+")
+    data = json.load(file)
+    for i in range(len(data)):
+        if data[i]["module_ID"] == module_ID:
+            data.pop(i)
+            file.seek(0)
+            file.truncate()
+            json.dump(data,file)
+            file.close()
+            return True
+    return False
+
+# CRUD - Create Read Update Delete
 
 
 if __name__ == "__main__":
